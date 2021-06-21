6fb2e8989b77dca931ffdb57592db41776e7f02c
douglasshipp <doug.shipp@atomicobject.com>
2021-06-20 23:46:49 -0400

    Refactored axios get request and added display logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,21 @@
 import React, {useEffect, useState} from "react";
 import "./App.css";
 import axios from "axios";
+import Web3 from 'web3';
 
 declare let window: any;
-let Web3 = require('web3');
 
-const fetchData = (userAccount: String) => {
-    const url = `https://api.opensea.io/api/v1/assets?owner=${userAccount}&order_direction=desc&offset=0&limit=20`;
-    const data = axios.get(url)
-        .then(res => {
-            console.log(res.data)
-                const {assets} = res.data
-                console.log(assets)
-                assets.forEach()
-                return assets
-            }
-        )
-        .catch(err => console.error('error:' + err));
-    console.log(data)
+type Image = {
+    id: string,
+    tokenId: string,
+    name: string,
+    image_url: string,
+    description: string,
 }
 
 const getAccount = async () => {
-
-
-    Web3 = window.web3
-    if (window.ethereum !== 'undefined') {
-        Web3 = new Web3(window.ethereum)
+    if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum)
         await window.ethereum.enable()
     } else if (window.web3) {
         window.web3 = new Web3(window.web3.currentProvider)
@@ -33,35 +23,55 @@ const getAccount = async () => {
         window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
     }
 
-    const account = await Web3.eth.getAccounts().then()
-    //setUserAccount(account)
-    return account
+    return await window.web3.eth.getAccounts().then()
 }
 
 export default function App(): JSX.Element {
-    const [images, setImages] = useState([]);
+    const [loading, setLoading] = useState(false)
+    const [images, setImages] = useState<Image[]>([]);
     const [userAccount, setUserAccount] = useState("")
 
-    const account = getAccount()
-
     useEffect(() => {
+        if (!loading) {
+            setLoading(true)
 
-         fetchData(account).then( image => {
-            setImages(image.imageUrl);
-        });
-    }, [account])
+            getAccount().then(account => {
+                setUserAccount(account)
+            })
 
-    return (
-        <div>
-            <h1>{userAccount}</h1>
-            {images.map((image, imageIdx) => {
-                return(
-                    <img src={image} key={imageIdx} alt="text" />
-                )
-            }
-            )}
-        </div>
-    )
+            fetchData(userAccount)
+        }
+    },[loading, userAccount])
+
+
+    const fetchData = async (userAccount: string) => {
+        const url = `https://api.opensea.io/api/v1/assets?owner=${userAccount}&order_direction=desc&offset=0&limit=20`;
+        const {data} = await axios.get(url)
+        const assets = data.assets
+        setImages(assets)
+        console.log("assets:", assets)
+    }
+
+        return (
+            <div className='App'>
+                <h1>{userAccount}</h1>
+                <div className="body">
+                    {images.length > 0 ? (
+                        <div className="content">
+                            {images.map((image,imageIdx) => (
+                                <div className="wrapper">
+                                <img src={image.image_url} key={imageIdx} alt="text"/>
+                                <h2>{image.name}</h2>
+                                <p>{image.description}</p>
+                                </div>
+                            ))}
+                        </div>
+                    ) : (
+                        <p className="loading">Loading... </p>
+                    )}
+                </div>
+            </div>
+        )
 }
 /*render() {
     if (!this.state.account) {
